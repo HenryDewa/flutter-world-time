d13a7a29150724d29e83681955cae0d78c9055e8
henry <henry.finasmart@gmail.com>
2019-12-31 01:39:21 +0700

    add world time app by flutter.

---
+++ lib/pages/loading.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  Loading({Key key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void getData() async {
+    Response response =
+        await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location' : instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time,
+      'isDayTime' : instance.isDayTime
+    });
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingGrid(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
